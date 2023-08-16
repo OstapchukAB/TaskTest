9d60166f1e507a4f68645d01a2b964effcc6432a
OstapchukAB <antbg@yandex.ru>
2023-08-16 17:11:55 +0300

    Parallel.Invoke

--- Program.cs
+++ Program.cs
@@ -22,35 +22,85 @@
 #endregion
 
 #region Example 02
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
+//using System.Runtime.CompilerServices;
+//using System.Threading.Tasks;
 
-(string Name,DateTime Dt) tt;
-var tasks = new Task<(string Name, DateTime Dt)>[3];
-for (var i = 0; i < tasks.Length; i++)
-{
-    var j = i;
-    tasks[i] = new(() =>
+//(string Name,DateTime Dt) tt;
+//var tasks = new Task<(string Name, DateTime Dt)>[3];
+//for (var i = 0; i < tasks.Length; i++)
+//{
+//    var j = i;
+//    tasks[i] = new(() =>
+//    {
+//        Console.WriteLine($"task {j} run {string.Format("{0:o}",DateTime.Now)}");
+//        Thread.Sleep(2000);
+//        tt = ($"Finish task {j} {string.Format("{0:o}", DateTime.Now)}", DateTime.Now);
+//        return tt;
+//    });
+//    tasks[i].Start();
+//}
+////Task.WaitAny(tasks);    
+////Console.WriteLine(task.Result);
+////Console.WriteLine(tasks[0].Result);
+//Task.WaitAll(tasks);
+//foreach (var task in tasks.OrderByDescending(x=>x.Result.Dt.Ticks))
+//{
+//    //Thread.Sleep(1000);
+//    Console.WriteLine(task.Result);
+//}
+
+////Console.WriteLine($"task.IsCompletedSuccessfully: {task.IsCompletedSuccessfully}");
+//Console.WriteLine($"Завершение метода Main {DateTime.Now}");
+
+
+#endregion
+
+#region continuation task
+//Task task1 = new Task(() =>
+//{
+//    Console.WriteLine($"Id задачи (task1): {Task.CurrentId}");
+//});
+
+//// задача продолжения - task2 выполняется после task1
+//Task task2 = task1.ContinueWith(PrintTask);
+
+//task1.Start();
+
+//// ждем окончания второй задачи
+//task2.Wait();
+//Console.WriteLine("Конец метода Main");
+
+
+//void PrintTask(Task t)
+//{
+//    Console.WriteLine($"Id задачи (task2): {Task.CurrentId}");
+//    Console.WriteLine($"Id предыдущей задачи (task1): {t.Id}");
+//    Thread.Sleep(3000);
+//}
+#endregion
+
+#region Parallel.Invoke
+// метод Parallel.Invoke выполняет три метода
+Parallel.Invoke(
+    Print,
+    () =>
     {
-        Console.WriteLine($"task {j} run {string.Format("{0:o}",DateTime.Now)}");
-        Thread.Sleep(2000);
-        tt = ($"Finish task {j} {string.Format("{0:o}", DateTime.Now)}", DateTime.Now);
-        return tt;
-    });
-    tasks[i].Start();
-}
-//Task.WaitAny(tasks);    
-//Console.WriteLine(task.Result);
-//Console.WriteLine(tasks[0].Result);
-Task.WaitAll(tasks);
-foreach (var task in tasks.OrderByDescending(x=>x.Result.Dt.Ticks))
+        Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+        Thread.Sleep(3000);
+    },
+    () => Square(5)
+);
+Console.WriteLine($"Завершение метода Main");
+void Print()
 {
-    //Thread.Sleep(1000);
-    Console.WriteLine(task.Result);
+    Console.WriteLine($"Выполняется задача Print {Task.CurrentId}");
+    Thread.Sleep(3000);
 }
-
-//Console.WriteLine($"task.IsCompletedSuccessfully: {task.IsCompletedSuccessfully}");
-Console.WriteLine($"Завершение метода Main {DateTime.Now}");
-
-
+// вычисляем квадрат числа
+void Square(int n)
+{
+    Console.WriteLine($"Выполняется задача Square {Task.CurrentId}");
+    Thread.Sleep(3000);
+    Console.WriteLine($"Результат {n * n}");
+}
 #endregion
